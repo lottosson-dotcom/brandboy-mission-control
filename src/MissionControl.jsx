9854cbc04cd166a4ac7ca1aaeab83087104b92e6
lottosson-dotcom <lottosson@gmail.com>
2026-07-05 12:16:48 +1000

    Update MissionControl.jsx

--- src/MissionControl.jsx
+++ src/MissionControl.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
-import { saveMemory, getMemories } from "./lib/agentMemory";
-import ActivityLog from "./components/ActivityLog";
+import { saveMemory, getMemories } from "./lib/agentMemory";js";
+import ActivityLog from "./components/ActivityLog.jsx";
 const C = {
   black:   "#0A0A0A",
   charcoal:"#141414",
